a910318c955a0a6d5e7a9a3b9ac5e7d8b708df6d
FluffyGameDev <71411509+FluffyGameDev@users.noreply.github.com>
2021-12-16 09:13:18 +0100

    Day 15 part 2 attempt

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -12,6 +12,11 @@ static constexpr size_t K_GRID_WIDTH{ 10 };
 static constexpr size_t K_GRID_HEIGHT{ 10 };
 static constexpr size_t K_GRID_CELL_COUNT{ K_GRID_WIDTH * K_GRID_HEIGHT };
 
+static constexpr size_t K_GRID_EXTENSION{ 5 };
+static constexpr size_t K_EXTENDED_GRID_WIDTH{ K_GRID_WIDTH * K_GRID_EXTENSION };
+static constexpr size_t K_EXTENDED_GRID_HEIGHT{ K_GRID_HEIGHT * K_GRID_EXTENSION };
+static constexpr size_t K_EXTENDED_GRID_CELL_COUNT{ K_EXTENDED_GRID_WIDTH * K_EXTENDED_GRID_HEIGHT };
+
 bool ReadInput(std::vector<u8>& grid)
 {
     static const char* inputFile{ "input.txt" };
@@ -60,11 +65,11 @@ using CellQueue = std::priority_queue<CellSearchInfo, std::vector<CellSearchInfo
 inline void TryVisitCell(CellQueue& cellQueue,
                          std::vector<u32>& cellBestScore,
                          const std::vector<u8>& grid,
-                         u32 cumulativeScore, u8 x, u8 y)
+                         u32 cumulativeScore, u8 x, u8 y, u32 w, u32 h)
 {
-    if (x < K_GRID_WIDTH && y < K_GRID_HEIGHT)
+    if (x < w && y < h)
     {
-        u32 index{ x + y * K_GRID_WIDTH };
+        u32 index{ x + y * w };
         cumulativeScore += grid[index];
         if (cumulativeScore < cellBestScore[index])
         {
@@ -74,32 +79,49 @@ inline void TryVisitCell(CellQueue& cellQueue,
     }
 }
 
-u32 ComputeBestPathScore(const std::vector<u8>& grid)
+u32 ComputeBestPathScore(const std::vector<u8>& grid, u32 w, u32 h)
 {
     CellQueue cellQueue(CompareCells);
     std::vector<u32> cellBestScore(grid.size(), std::numeric_limits<u32>::max());
 
-    TryVisitCell(cellQueue, cellBestScore, grid, 0, 0, 0);
+    TryVisitCell(cellQueue, cellBestScore, grid, 0, 0, 0, w, h);
 
     while (!cellQueue.empty())
     {
         CellSearchInfo cellInfo{ cellQueue.top() };
         cellQueue.pop();
 
-        if (cellInfo.x == K_GRID_WIDTH - 1 && cellInfo.y == K_GRID_HEIGHT - 1)
+        if (cellInfo.x != w - 1 || cellInfo.y != h - 1)
         {
-
-        }
-        else
-        {
-            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x + 1, cellInfo.y);
-            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x - 1, cellInfo.y);
-            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x, cellInfo.y + 1);
-            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x, cellInfo.y - 1);
+            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x + 1, cellInfo.y, w, h);
+            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x - 1, cellInfo.y, w, h);
+            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x, cellInfo.y + 1, w, h);
+            TryVisitCell(cellQueue, cellBestScore, grid, cellInfo.CumulativeScore, cellInfo.x, cellInfo.y - 1, w, h);
         }
     }
 
-    return cellBestScore[K_GRID_CELL_COUNT - 1] - grid[0];
+    return cellBestScore[w * h - 1] - grid[0];
+}
+
+void BuildExtendedGrid(const std::vector<u8>& baseGrid, std::vector<u8>& extendedGrid)
+{
+    for (u32 index = 0; index < K_GRID_CELL_COUNT; ++index)
+    {
+        u32 x{ index % K_GRID_WIDTH};
+        u32 y{ index / K_GRID_WIDTH};
+
+        for (u32 j = 0; j < K_GRID_EXTENSION; ++j)
+        {
+            u32 extendedY{ y + j * K_GRID_HEIGHT };
+            for (u32 i = 0; i < K_GRID_EXTENSION; ++i)
+            {
+                u32 extendedX{ x + i * K_GRID_WIDTH };
+                u32 extendedIndex{ extendedX + extendedY * K_EXTENDED_GRID_WIDTH };
+                extendedGrid[extendedIndex] = (baseGrid[index] + i + j);
+                if (extendedGrid[extendedIndex] > 9) { extendedGrid[extendedIndex] -= 9; }
+            }
+        }
+    }
 }
 
 int main()
@@ -107,8 +129,14 @@ int main()
     std::vector<u8> grid(K_GRID_CELL_COUNT, 0);
     if (ReadInput(grid))
     {
-        u32 bestScore{ ComputeBestPathScore(grid) };
-        fmt::print("Best Score: {}.\n", bestScore);
+        //u32 bestScore{ ComputeBestPathScore(grid, K_GRID_WIDTH, K_GRID_HEIGHT) };
+        //fmt::print("Best Score: {}.\n", bestScore);
+
+        // Does not work for part 2. Must optimize!
+        std::vector<u8> extendedGrid(K_EXTENDED_GRID_CELL_COUNT, 0);
+        BuildExtendedGrid(grid, extendedGrid);
+        u32 bestExtendedScore{ ComputeBestPathScore(extendedGrid, K_EXTENDED_GRID_WIDTH, K_EXTENDED_GRID_HEIGHT) };
+        fmt::print("Best Extended Score: {}.\n", bestExtendedScore);
     }
     else
     {
